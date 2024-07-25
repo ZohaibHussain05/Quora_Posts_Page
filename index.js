9f740ed75f9b4ac7a586be2a1d0c71d6bdf372e7
Zohaib Hussain <chzohaibjutt300@gmail.com>
2024-07-25 20:36:55 +0500

    [ADD] Initial files. [ADD] express and ejs

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"views"));
+
+app.use(express.static( path.join(__dirname,"public")));
+
+let posts = [
+    {
+        username : "zohaib05",
+        content: "I love coding"
+    },
+    {
+        username: "Ali Ahmed",
+        content: "I got my first job",
+    },
+    {
+        username: "AbdurRehman",
+        content: "check my new project"
+    }
+]
+
+const port = 8080;
+app.listen(port , () => {
+    console.log(`App is listening on port ${port}`);
+
+})
+
+app.get("/posts", (req, res) =>{
+    res.render("index.ejs" , {posts});
+})
