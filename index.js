64b9c4aeeccce4819e62f2e40ee1b58f5bb57da9
Zohaib Hussain <chzohaibjutt300@gmail.com>
2024-07-28 07:37:27 +0500

    [ADD] Delete post using DELETE Api

--- index.js
+++ index.js
@@ -69,3 +69,10 @@ app.get("/posts/:id/edit" , (req, res) => {
     let post = posts.find((p) => id == p.id);
     res.render("edit.ejs", {post})
 });
+
+app.delete("/posts/:id" , (req,res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id == p.id);
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+})
