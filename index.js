ef622ba111a7f84f8a18c3982d64e6ae824e1579
Zohaib Hussain <chzohaibjutt300@gmail.com>
2024-07-28 07:24:42 +0500

    [ADD] Edit post option using patch api

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const {v4: uuidv4} = require('uuid');
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"views"));
@@ -52,4 +54,18 @@ app.post("/posts", (req , res) => {
     let {username, content} = req.body;
     posts.push({id:uuidv4(),username , content});
     res.redirect("/posts");
+});
+
+app.patch("/posts/:id", (req,res) => {
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id == p.id);
+    post.content = newContent;
+    res.redirect("/posts");
 })
+
+app.get("/posts/:id/edit" , (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id == p.id);
+    res.render("edit.ejs", {post})
+});
